7cc871e6a71d1b77702cbc778b088698bec36800
Anna Navoznova <anna.navoznova@gmail.com>
2022-02-08 20:47:35 +0300

    feat: creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { EditComponent } from './shopping-list/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
