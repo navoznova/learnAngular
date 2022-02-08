f3653fd6beb79967044cabe2befc46e3e643ee5b
Anna Navoznova <anna.navoznova@gmail.com>
2022-02-08 11:00:15 +0300

    feat: move fales to separate folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningComponent } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
 import { FormsModule } from '@angular/forms';
-import { PracticingDirectivesComponent } from './practicing-directives/practicing-directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningComponent,
-    SuccessComponent,
-    PracticingDirectivesComponent
   ],
   imports: [
     BrowserModule,
