e43f3a4d83ef78ff5c1225abfe1cf4a135683490
Anna Navoznova <anna.navoznova@gmail.com>
2022-11-25 13:50:49 +0800

    feat: add link for a button

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Recipe } from '../recipe.module';
 import { RecipeService } from '../recipe.service';
 
@@ -10,11 +11,15 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [];
 
-    constructor(private recipeService: RecipeService) {
-    }
+    constructor(private recipeService: RecipeService,
+                private router: Router) {}
 
     ngOnInit(): void {
         this.recipes = this.recipeService.getRecipes();
+    }
+
+    onRouteNewRecipePage() {
+        this.router.navigate(['/shopping-list']);
     }
 
 }
