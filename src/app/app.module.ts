d54fb1c00257f9172060db508fa487baf0031314
Anna Navoznova <anna.navoznova@gmail.com>
2022-12-19 12:17:34 +0800

    feat: add recipe module and rout-model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { EditComponent } from './shopping-list/edit/edit.component';
-import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { DropDownDirective } from './shared/drop-down.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeModule } from './recipes/recipe.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipesComponent,
     EditComponent,
-    RecipeDetailsComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
     DropDownDirective,
-    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
@@ -35,6 +26,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RecipeModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
---
+++ src/app/recipes/recipe.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes.component';
+import { RecipeRoutingModule } from './recipe-routing.module';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeEditComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RecipeRoutingModule
+  ],
+  exports:[
+    RecipesComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeEditComponent
+  ],
+})
+export class RecipeModule { }
