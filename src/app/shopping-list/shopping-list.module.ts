8b98e4071d0a6b5d45e44dc6473d70a3f004a1c4
Anna Navoznova <anna.navoznova@gmail.com>
2022-12-21 13:02:48 +0800

    feat: split routing for the shopping list

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 import { EditComponent } from "./edit/edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
 
@@ -12,6 +13,9 @@ import { ShoppingListComponent } from "./shopping-list.component";
     imports:[
         CommonModule,
         FormsModule,
-    ]
+        RouterModule.forChild([
+            { path: 'shopping-list', component: ShoppingListComponent },
+        ]),
+    ],
 })
 export class ShoppingListModule {}
